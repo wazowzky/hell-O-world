a7209a9823470e77d715602352dd90995f188fb2
wazowzky <53339828+wazowzky@users.noreply.github.com>
2019-07-26 16:06:09 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,15 @@
+<?php
+date_default_timezone_set('Europe/Brussels');
+?>
 <html>
-        <head>
-        </head>
-        
-        <body>
-                <?php
-                        echo  date("m/d/Y H:i:s");
-                ?>
-        </body>
+	<head>
+	</head>
+	
+	<body>
+		<h1 id="div1" style="text-align:right;background:linear-gradient(#00ffe3 15%, #1200ff);-webkit-background-clip: text;-webkit-text-fill-color: transparent;">
+		<?php
+			echo date("d/m/Y H:i:s");
+		?>
+		</h1>
+	</body>
 </html>
