1ebd75f2951af7bfa5f434e5130ad7829fb17006
wazowzky <53339828+wazowzky@users.noreply.github.com>
2019-07-26 14:31:58 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,10 @@
+<html>
+        <head>
+        </head>
+        
+        <body>
+                <?php>
+                        echo  date("m/d/Y H:i:s");
+                <?>
+        </body>
+</html>
